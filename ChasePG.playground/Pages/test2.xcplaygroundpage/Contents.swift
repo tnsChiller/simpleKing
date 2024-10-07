b86b2513dc099304143d8ba0cdc8384b3d6360e1
tnsChiller <ikivanc4@gmail.com>
2024-10-07 20:12:03 +0100

    -

--- ChasePG.playground/Pages/test2.xcplaygroundpage/Contents.swift
+++ ChasePG.playground/Pages/test2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,5 @@
-//: [Previous](@previous)
+import Cocoa
 
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+var hands: [String: [Int]] = ["p1":[], "p2":[], "p3":[], "p4":[]]
+let keys = Array(hands.keys)
+print(hands[keys[0]])
