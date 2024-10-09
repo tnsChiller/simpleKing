435a31db5caaa653c8ab83474c16c7549444174d
tnsChiller <ikivanc4@gmail.com>
2024-10-09 21:00:06 +0100

    -

--- ChasePG.playground/Pages/test.swift
+++ ChasePG.playground/Pages/test.swift
@@ -8,6 +8,7 @@ class kingGameState {
     var banks: [Int] = Array(repeating: -1, count: 52)
     let pIdx: [Int] = [0, 13, 26, 39, 52]
     let erkekIdx: [Int] = [10, 12, 23, 25, 36, 38, 49, 51]
+    let kizIdx: [Int] = [11, 24, 37, 50]
     var (turn, round, gameNo): (Int, Int, Int) = (0, 0, 0)
     var (suit, type, start): (Int, Int, Int) = (-1, -1, -1)
     var kupaDustu: Bool = false
@@ -44,29 +45,46 @@ class kingGameState {
     func step() {
         let anchor: Int = (start + turn) % 4
         let range: ClosedRange<Int> = self.pIdx[anchor]...pIdx[anchor + 1] - 1
-
+        // Pre Action
         var hasSuit: Bool = false
+        var midMax: Int = -1
         if self.turn != 0 {
             for idx: ClosedRange<Int>.Element in range {
                 if self.deck[idx] / 13 == self.suit && !self.played[idx] {
                     hasSuit = true
                 }
             }
+
+            for idx: ClosedRange<Int>.Element in 0...51 {
+                if self.middle[idx] {
+                    if self.deck[idx] % 13 > midMax
+                        && self.deck[idx] / 13 == self.suit
+                    {
+                        midMax = self.deck[idx] % 13
+                    }
+                }
+            }
         }
 
-        if self.type == 0 || self.type == 1 { // El Almaz & Son Iki Almaz
-
-        } else if self.type == 2 { // Kupa Almaz
-            var hasKupa = false
+        if self.type == 0 || self.type == 1 {  // El Almaz & Son Iki Almaz
+            for idx: ClosedRange<Int>.Element in range {
+                if !self.played[idx] {
+                    if self.turn == 0 || !hasSuit {
+                        self.playable[idx] = true
+                    } else {
+                        if self.deck[idx] / 13 == self.suit {
+                            self.playable[idx] = true
+                        }
+                    }
+                }
+            }
+        } else if self.type == 2 {  // Kupa Almaz
+            var hasKupa: Bool = false
             var onlyKupa: Bool = true
-            var maxKupa: Int = 0
             for idx: ClosedRange<Int>.Element in range {
                 if !self.played[idx] {
                     if self.deck[idx] / 13 == 1 {
                         hasKupa = true
-                        if self.deck[idx] % 13 > maxKupa {
-                            maxKupa = self.deck[idx] % 13
-                        }
                     } else {
                         onlyKupa = false
                     }
@@ -75,105 +93,82 @@ class kingGameState {
 
             for idx: ClosedRange<Int>.Element in range {
                 if !self.played[idx] {
-                    if hasKupa {
-                        if onlyKupa {
-                            if self.deck[idx] % 13 == maxKupa {
-                                self.playable[idx] = true
-                            }
-                        } else if kupaDustu {
+                    if self.turn == 0 || !hasSuit {
+                        if self.kupaDustu || onlyKupa {
                             self.playable[idx] = true
                         } else {
                             if self.deck[idx] / 13 != 1 {
                                 self.playable[idx] = true
                             }
                         }
+                    } else {
+                        if self.deck[idx] / 13 == self.suit {
+                            self.playable[idx] = true
+                        }
                     }
                 }
             }
 
-        } else if self.type == 3 {
+        } else if self.type == 3 || self.type == 4{  // Erkek Almaz Kiz Almaz
+            let ditchableRange: [Int] = self.type == 3 ? self.erkekIdx: self.kizIdx
+            var hasDitchable: Bool = false
+            var ditchableIdx: Int = -1
+            var ditchable: Int = -1
+            for idx: ClosedRange<Int>.Element in range {
+                if !self.played[idx] {
+                    if self.turn == 0 {
+                        self.playable[idx] = true
+                    } else if hasSuit {
+                        if self.deck[idx] / 13 == self.suit {
+                            for idxx: Int in ditchableRange {
+                                if idx == idxx && self.deck[idx] % 13 > midMax {
+                                    hasDitchable = true
+                                    if self.deck[idx] % 13 > ditchable {
+                                        ditchableIdx = idx
+                                        ditchable = self.deck[idx] % 13
+                                    }
+                                }
+                            }
 
-        } else if self.type == 4 {
+                            if hasDitchable {
+                                for idxx: ClosedRange<Int>.Element in range {
+                                    self.playable[idxx] = false
+                                }
+                                self.playable[ditchableIdx] = true
+                                break
+                            } else {
+                                self.playable[idx] = true
+                            }
+                        }
+                    } else {
+                        for idxx: Int in ditchableRange {
+                            if idx == idxx && self.deck[idx] % 13 > midMax {
+                                hasDitchable = true
+                                if self.deck[idx] % 13 > ditchable {
+                                    ditchableIdx = idx
+                                    ditchable = self.deck[idx] % 13
+                                }
+                            }
+                        }
+
+                        if hasDitchable {
+                            for idxx: ClosedRange<Int>.Element in range {
+                                self.playable[idxx] = false
+                            }
+                            self.playable[ditchableIdx] = true
+                            break
+                        } else {
+                            self.playable[idx] = true
+                        }
+                    }
+                }
+            }
 
         } else if self.type == 5 {
 
         } else if self.type == 6 || self.type == 7 || self.type == 8 || self.type == 9 {
             var koz: Int = self.type - 6
 
-        }
-
-        var hasErkek: Bool = false
-        if self.type == 2 {
-            for idx: ClosedRange<Int>.Element in range {
-                for idxx: Int in erkekIdx {
-                    if idx == idxx {
-                        hasErkek = true
-                    }
-                }
-            }
-        }
-
-        var midMax: Int = -1
-        for idx: Int in 0...51 {
-            if self.middle[idx] {
-                if self.deck[idx] % 13 > midMax {
-                    midMax = self.deck[idx]
-                }
-            }
-        }
-
-        if self.turn == 0 {
-            for idx: ClosedRange<Int>.Element in range {
-                if !self.played[idx] {
-                    if self.type == 1 && hasKupa {
-                        if onlyKupa {
-                            if self.deck[idx] % 13 == maxKupa {
-                                self.playable[idx] = true
-                            }
-                        } else if kupaDustu {
-                            self.playable[idx] = true
-                        } else {
-                            if self.deck[idx] / 13 != 1 {
-                                self.playable[idx] = true
-                            }
-                        }
-                    } else {
-                        self.playable[idx] = true
-                    }
-                }
-            }
-        } else if hasSuit {
-            for idx: ClosedRange<Int>.Element in range {
-                if !self.played[idx] {
-                    if self.type == 2 {
-
-                    } else if self.deck[idx] / 13 == self.suit {
-                        self.playable[idx] = true
-                    }
-                }
-            }
-        } else if self.type == 1 {  // Kupa Almaz
-            for idx: ClosedRange<Int>.Element in range {
-                if !self.played[idx] {
-                    if hasKupa {
-                        if onlyKupa {
-                            if self.deck[idx] % 13 == maxKupa {
-                                self.playable[idx] = true
-                            }
-                        } else {
-                            if self.deck[idx] / 13 == 1 {
-                                self.playable[idx] = true
-                            }
-                        }
-                    } else {
-                        self.playable[idx] = true
-                    }
-                }
-            }
-        } else {
-            for idx: ClosedRange<Int>.Element in range {
-                if !self.played[idx] { self.playable[idx] = true }
-            }
         }
 
         var playableIndices: [Int] = []
--- TestPG.playground/Contents.swift
+++ TestPG.playground/Contents.swift
@@ -1,2 +1,13 @@
-var a = [1, 2, 3, 4]
-a.contains
+var a = 0
+for i in [1, 2, 3, 4, 5] {
+    print ("i = \(i)")
+    for j in [6, 7, 8, 9] {
+        print ("j = \(j)")
+        if i == 4 {
+            if j == 8 {
+                break
+            }
+            break
+        }
+    }
+}
